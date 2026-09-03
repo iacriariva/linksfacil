a90165aaa1fc81eb4c7bf6eebb827fe9b0f32291
iacriariva <iacriariva@proton.me>
2026-09-03 06:17:21 -0300

    Setup project configuration files (package.json, tsconfig, tailwind, etc)

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#10B981',
+        danger: '#EF4444',
+      },
+    },
+  },
+  plugins: [],
+};
